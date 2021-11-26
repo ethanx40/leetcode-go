88f193d981e9eed288c2a3e1bcc5855e9376f487
ethanx40 <93695126+ethanx40@users.noreply.github.com>
2021-11-26 16:33:43 +0800

    Update binary_tree_inorder_traversal.go

--- binary_tree_inorder_traversal.go
+++ binary_tree_inorder_traversal.go
@@ -32,6 +32,8 @@ Output: [1,2]
  *     Right *TreeNode
  * }
  */
+
+// recursive implementation
 func inorderTraversal(root *TreeNode) []int {
     if root == nil {
         return nil
@@ -41,4 +43,25 @@ func inorderTraversal(root *TreeNode) []int {
     middle := append(left, root.Val)
     right := inorderTraversal(root.Right)
     return append(middle, right...)
+}
+
+// iteratively implementation
+func inorderTraversal(root *TreeNode) []int {
+    var result []int
+    var stack []*TreeNode
+    
+    for root != nil || len(stack) > 0 {
+        if root != nil {
+            stack = append(stack, root)
+            root = root.Left
+            continue
+        }
+        
+        stackLen := len(stack)
+        root = stack[stackLen-1]
+        stack = stack[0:stackLen-1]
+        result = append(result, root.Val)
+        root = root.Right
+    }
+    return result
 }
